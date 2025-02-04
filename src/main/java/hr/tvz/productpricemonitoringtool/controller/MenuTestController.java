e88826f516f61fa785bf2847617438d716f7710b
Leo <36726619+leostricak22@users.noreply.github.com>
2025-02-05 00:25:28 +0100

    feat: add new menu design

---
+++ src/main/java/hr/tvz/productpricemonitoringtool/controller/MenuTestController.java
@@ -0,0 +1,13 @@
+package hr.tvz.productpricemonitoringtool.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MenuTestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MenuTestController.class);
+
+    public void initialize() {
+        logger.info("MenuTestController initialized");
+    }
+}
