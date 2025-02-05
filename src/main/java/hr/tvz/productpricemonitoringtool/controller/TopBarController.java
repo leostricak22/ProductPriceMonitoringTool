f1eb3a5b051b149df5078ac77d48a1857b182715
Leo <36726619+leostricak22@users.noreply.github.com>
2025-02-05 22:54:33 +0100

    feat: top bar add profile image and notification icon

---
+++ src/main/java/hr/tvz/productpricemonitoringtool/controller/TopBarController.java
@@ -0,0 +1,26 @@
+package hr.tvz.productpricemonitoringtool.controller;
+
+import hr.tvz.productpricemonitoringtool.util.SceneLoader;
+import hr.tvz.productpricemonitoringtool.util.Session;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+public class TopBarController {
+
+    @FXML public Circle userProfilePictureCircle;
+    @FXML public Rectangle notificationIconRectangle;
+
+    public void initialize() {
+        Image image = Session.getUserProfilePicture();
+        userProfilePictureCircle.setFill(new ImagePattern(image));
+
+        notificationIconRectangle.setFill(new ImagePattern(new Image("file:src/main/resources/hr/tvz/productpricemonitoringtool/images/icons/notifications.png")));
+    }
+
+    public void handleUserEditRedirect() {
+        SceneLoader.loadScene("user_edit", "Edit User");
+    }
+}
