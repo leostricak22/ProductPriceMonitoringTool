e46a5477297e87b12608bfe9de1b0efae61fd7e4
Leo <36726619+leostricak22@users.noreply.github.com>
2025-02-06 00:20:24 +0100

    feat: add new design to all views

--- src/main/java/hr/tvz/productpricemonitoringtool/controller/MenuTestController.java
+++ src/main/java/hr/tvz/productpricemonitoringtool/controller/MenuTestController.java
@@ -1,13 +1,42 @@
 package hr.tvz.productpricemonitoringtool.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import hr.tvz.productpricemonitoringtool.model.Company;
+import hr.tvz.productpricemonitoringtool.util.SceneLoader;
+import hr.tvz.productpricemonitoringtool.util.Session;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.Optional;
 
 public class MenuTestController {
 
-    private static final Logger logger = LoggerFactory.getLogger(MenuTestController.class);
+    private Optional<Company> selectedCompany;
+
+    @FXML public Label companyNameLabel;
 
     public void initialize() {
-        logger.info("MenuTestController initialized");
+        selectedCompany = Session.getSelectedCompany();
+        if (selectedCompany.isPresent()) {
+            companyNameLabel.setVisible(true);
+            companyNameLabel.setText("Currently in: " + selectedCompany.get().getName());
+        } else {
+            companyNameLabel.setVisible(false);
+        }
+    }
+
+    public void handleDashboardRedirect() {
+        if (selectedCompany.isPresent()) {
+            SceneLoader.loadScene("company_dashboard", selectedCompany.get().getName());
+            return;
+        }
+
+        SceneLoader.loadScene("dashboard", "Dashboard");
+    }
+
+    public void handleProductSearchRedirect() {
+        SceneLoader.loadProductSearchScene(
+                "product_search",
+                "Product Search",
+                Optional.empty());
     }
 }
