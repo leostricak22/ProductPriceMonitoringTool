f0ce9e812d28196df98c86f2a49c779fb123e009
Leo <36726619+leostricak22@users.noreply.github.com>
2025-02-06 00:22:22 +0100

    feat: convert MenuTest to Menu

--- src/main/java/hr/tvz/productpricemonitoringtool/controller/MenuController.java
+++ src/main/java/hr/tvz/productpricemonitoringtool/controller/MenuController.java
@@ -5,32 +5,22 @@ import hr.tvz.productpricemonitoringtool.util.SceneLoader;
 import hr.tvz.productpricemonitoringtool.util.Session;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
 
 import java.util.Optional;
 
 public class MenuController {
 
-    @FXML public Circle userProfilePictureCircle;
-    @FXML public Label companyNameTextLabel;
-    @FXML public Label companyNameLabel;
-
     private Optional<Company> selectedCompany;
 
-    public void initialize() {
-        Image image = Session.getUserProfilePicture();
-        userProfilePictureCircle.setFill(new ImagePattern(image));
+    @FXML public Label companyNameLabel;
 
+    public void initialize() {
         selectedCompany = Session.getSelectedCompany();
         if (selectedCompany.isPresent()) {
             companyNameLabel.setVisible(true);
-            companyNameTextLabel.setVisible(true);
-            companyNameLabel.setText(selectedCompany.get().getName());
+            companyNameLabel.setText("Currently in: " + selectedCompany.get().getName());
         } else {
             companyNameLabel.setVisible(false);
-            companyNameTextLabel.setVisible(false);
         }
     }
 
@@ -41,10 +31,6 @@ public class MenuController {
         }
 
         SceneLoader.loadScene("dashboard", "Dashboard");
-    }
-
-    public void handleUserEditRedirect() {
-        SceneLoader.loadScene("user_edit", "Edit User");
     }
 
     public void handleProductSearchRedirect() {
