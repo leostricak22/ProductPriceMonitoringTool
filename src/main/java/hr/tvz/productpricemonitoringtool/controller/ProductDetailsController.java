ca4c5f5d38ea8cdb9714ddb5f13895de751bd63b
Leo <36726619+leostricak22@users.noreply.github.com>
2025-02-16 18:01:46 +0100

    feat: Finish product details scene

--- src/main/java/hr/tvz/productpricemonitoringtool/controller/ProductDetailsController.java
+++ src/main/java/hr/tvz/productpricemonitoringtool/controller/ProductDetailsController.java
@@ -1,6 +1,7 @@
 package hr.tvz.productpricemonitoringtool.controller;
 
 import hr.tvz.productpricemonitoringtool.exception.DatabaseConnectionActiveException;
+import hr.tvz.productpricemonitoringtool.model.Company;
 import hr.tvz.productpricemonitoringtool.model.CompanyProduct;
 import hr.tvz.productpricemonitoringtool.model.Product;
 import hr.tvz.productpricemonitoringtool.repository.CompanyProductRepository;
@@ -8,13 +9,14 @@ import hr.tvz.productpricemonitoringtool.util.AlertDialog;
 import hr.tvz.productpricemonitoringtool.util.SceneLoader;
 import hr.tvz.productpricemonitoringtool.util.Session;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 
 public class ProductDetailsController {
@@ -23,8 +25,15 @@ public class ProductDetailsController {
     @FXML public Label productNameLabel;
     @FXML public Label productPriceLabel;
     @FXML public Label productCategoryLabel;
+    @FXML public Label companyProductPriceLabel;
+    @FXML public Label companyProductPriceTitleLabel;
+    @FXML public Button companyProductPriceButton;
+    @FXML public Button companyProductsSortButton;
+    @FXML public VBox companyProductsVBox;
 
     private final CompanyProductRepository companyProductRepository = new CompanyProductRepository();
+    private String sortType = "desc";
+    private List<CompanyProduct> companyProducts = new ArrayList<>();
 
     public void initialize() {
         if (Session.getSelectedProduct().isEmpty()) {
@@ -38,9 +47,8 @@ public class ProductDetailsController {
         productNameLabel.setText(selectedProduct.getName());
         productCategoryLabel.setText(selectedProduct.getCategory().getName());
 
-        Set<CompanyProduct> companyProducts = new HashSet<>();
         try {
-            companyProducts = companyProductRepository.findByProductId(selectedProduct.getId());
+            companyProducts = new ArrayList<>(companyProductRepository.findByProductId(selectedProduct.getId()));
         } catch (DatabaseConnectionActiveException e) {
             AlertDialog.showErrorDialog("Database connection active", "Please try again later.");
             SceneLoader.loadScene("dashboard", "Dashboard");
@@ -56,6 +64,59 @@ public class ProductDetailsController {
                 .max(BigDecimal::compareTo)
                 .orElse(BigDecimal.ZERO);
 
-        productPriceLabel.setText(lowestPrice.toString() + "€ - " + highestPrice.toString() + "€");
+        productPriceLabel.setText(lowestPrice + "€ - " + highestPrice + "€");
+
+        Optional<Company> selectedCompany = Session.getSelectedCompany();
+
+        if (selectedCompany.isPresent()) {
+            Optional<BigDecimal> price = companyProducts.stream()
+                    .filter(companyProduct -> Objects.equals(companyProduct.getCompany().getId(), selectedCompany.get().getId()))
+                    .map(companyProduct -> companyProduct.getPrice().value())
+                    .findFirst();
+
+            price.ifPresent(bigDecimal -> {
+                companyProductPriceLabel.setText(bigDecimal + "€");
+                companyProductPriceTitleLabel.setText("Price in " + selectedCompany.get().getName());
+            });
+
+            if (price.isEmpty()) {
+                companyProductPriceLabel.setVisible(false);
+                companyProductPriceTitleLabel.setVisible(false);
+                companyProductPriceButton.setVisible(false);
+            }
+        }
+
+        companyProducts.sort((cp1, cp2) -> cp2.getPrice().value().compareTo(cp1.getPrice().value()));
+        fillCompanyProductsVBox();
+    }
+
+    public void handleCompanySort() {
+        if (sortType.equals("asc")) {
+            sortType = "desc";
+            companyProductsSortButton.setText("Sort by price (asc)");
+            companyProducts.sort((cp1, cp2) -> cp2.getPrice().value().compareTo(cp1.getPrice().value()));
+        } else {
+            sortType = "asc";
+            companyProductsSortButton.setText("Sort by price (desc)");
+            companyProducts.sort((cp1, cp2) -> cp1.getPrice().value().compareTo(cp2.getPrice().value()));
+        }
+
+        fillCompanyProductsVBox();
+    }
+
+    public void fillCompanyProductsVBox() {
+        companyProductsVBox.getChildren().clear();
+
+        for (CompanyProduct companyProduct : companyProducts) {
+            Label companyNameLabel = new Label(companyProduct.getCompany().getName() + ": ");
+            companyNameLabel.getStyleClass().add("formLabel");
+
+            Label priceLabel = new Label(companyProduct.getPrice().value() + "€");
+            priceLabel.setStyle("-fx-font-weight: bold");
+            priceLabel.getStyleClass().add("formLabel");
+
+            HBox productBox = new HBox(companyNameLabel, priceLabel);
+            companyProductsVBox.getChildren().add(productBox);
+        }
     }
 }
